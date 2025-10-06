88fa018f372cb89a5fc7284c43a0575a07ac7ec7
kirank22 <kirankothapalli22@gmail.com>
2025-10-05 23:23:22 -0400

    Initial Commit

---
+++ LoveTap/LoveTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveTapApp.swift
+//  LoveTap
+//
+//  Created by Kiran Kothapalli on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LoveTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
